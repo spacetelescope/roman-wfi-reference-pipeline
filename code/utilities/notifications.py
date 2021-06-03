eb89d1d65ab19966cd50dabcf3f154480024a2d0
Tyler Desjardins <desjard@stsci.edu>
2021-06-03 14:50:17 -0400

    Updated docstring for notifications

--- code/utilities/notifications.py
+++ code/utilities/notifications.py
@@ -1,3 +1,7 @@
+"""
+Tools for sending notifications from the pipeline.
+"""
+
 import importlib.resources as pkg_resources
 import json
 import requests
@@ -6,7 +10,21 @@ import yaml
 from wfi_reference_pipeline import config
 
 
-def send_slack_message(message, config_file='slack_devtest.yml'):
+def send_slack_message(message, config_file='slack_dev.yml'):
+    """
+    Function for sending messages to a slack channel.
+
+    Inputs
+    ------
+    message (str): Message to send to the slack channel.
+    config_file (str; optional; default="slack_dev.yml"): YAML file containing
+        the configuration information for the slack integration including
+        the webhook URL and optional name/icon information.
+
+    Returns
+    -------
+    None
+    """
 
     with pkg_resources.open_text(config, config_file) as cf:
         slack_config = yaml.safe_load(cf)
