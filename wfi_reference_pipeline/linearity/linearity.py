a3a5012c782aaa7e84f07c7af73fd392e012703d
Javier Sanchez <jsanchez@stsci.edu>
2022-08-10 19:02:18 -0400

    working version

--- wfi_reference_pipeline/linearity/linearity.py
+++ wfi_reference_pipeline/linearity/linearity.py
@@ -30,8 +30,10 @@ class Linearity(ReferenceFile):
             self.meta['reftype'] = 'LINEARITY'
         else:
             pass
+        self.unc = None
 
-    def make_linearity(self, input_files, poly_order=5):
+    def make_linearity(self, input_files, poly_order=5, constrained=False,
+                       nframes_grid=10, use_unc=False, return_unc=False):
         """
         The method make_linearity() generates a linearity asdf file.
 
@@ -40,7 +42,11 @@ class Linearity(ReferenceFile):
         input_files: str or list of str; File or list of flat files to
                      use to compute the linearity.
         poly_order: integer; Polynomial order to use for the fits.
-
+        constrained: bool; If True, it returns the fit resulting fixing intercept
+                     to 0 and slope to 1.
+        nframes_grid: integer; Number of points in the grid to evaluate the fit.
+        use_unc: bool; If True, it uses the spread in the fits to compute the best-fit.
+        return_unc: bool; If True, it returns the uncertainty of the fit.
         Outputs
         -------
         af: asdf file tree: {meta, coeffs, dq}
@@ -52,21 +58,135 @@ class Linearity(ReferenceFile):
         # Check if the output file exists, and take appropriate action.
         self.check_output_file(self.outfile)
 
-        # Construct the linearity object from the data model.
-        linearityfile = rds.LinearityRef()
-        linearityfile['meta'] = self.meta
-        linearityfile['coeffs'] = self.data
-        nonlinear_pixels = np.where(self.mask == float('NaN'))
-        self.mask[nonlinear_pixels] = 2 ** 20  # linearity correction not available
-        linearityfile['dq'] = self.mask
         # Linearity files do not have data quality or error arrays.
+        if input_files is not None:
+            if isinstance(input_files, str):
+                # If it's a string, then it's a single file
+                coeffs, mask = self.fit_single(input_files, poly_order=poly_order,
+                                               constrained=constrained)
+                self.data = coeffs
+            elif isinstance(input_files, list):
+                # If it's a list, iterate
+                coeff_list = []
+                tmin = 1e24
+                tmax = -1e24
+                # Fit each image in the list and save the coefficients
+                for idx, fname in enumerate(input_files):
+                    aux_coeffs, mask, tt = self.fit_single(fname, poly_order=poly_order,
+                                                           constrained=constrained,
+                                                           return_time=True)
+                    if idx == 0:
+                        npx0 = aux_coeffs.shape[1]
+                        npx1 = aux_coeffs.shape[2]
+                    coeff_list.append(aux_coeffs)
+                    if np.min(tt) < tmin:
+                        tmin = np.min(tt)
+                    if np.max(tt) > tmax:
+                        tmax = np.max(tt)
+                # Set the grid to evaluate the polynomials between the min and max time
+                # TODO check if tmax should be the minimum of all the tmax's
+                t_grid = np.linspace(tmin, tmax, nframes_grid)
+                img_arr_all = np.zeros((nframes_grid, npx0, npx1))
+                if use_unc:
+                    img_arr_all_sq = np.zeros_like(img_arr_all)
+                # Here we compute the mean of the polynomial evaluated in a grid
+                # as suggested by S. Casertano
+                for coeff in coeff_list:
+                    if constrained:  # The different count rates are already taken into account
+                        img_arr_all += np.polyval(coeff, t_grid[:, None, None])
+                    else:
+                        # It the slope and intercept are not 1, and the count rates
+                        # are different, we have to normalize everything to the same
+                        # count rate.
+                        img_arr_all += (np.polyval(coeff, t_grid[:, None, None]) -
+                                        coeff[poly_order, :]) / coeff[poly_order-1, :]
+                    if use_unc:
+                        # This is to get the std as <mean^2> - <mean>^2
+                        if constrained:
+                            img_arr_all_sq += np.polyval(coeff, t_grid[:, None, None])**2
+                        else:
+                            img_arr_all_sq += ((np.polyval(coeff, t_grid[:, None, None]) -
+                                               coeff[poly_order, :])/coeff[poly_order-1, :])**2
 
-        # Add in the meta data and history to the ASDF tree.
-        af = asdf.AsdfFile()
-        af.tree = {'roman': linearityfile}
-        af.write_to(self.outfile)
+                img_arr_all /= len(coeff_list)  # <X>
+                # Use the spread between fits (not coefficients!) as uncertainty for the final fit
+                if use_unc:
+                    # Polyfit unfortunately does not like 2d weights, so
+                    # we have to compute this stuff by hand
+                    img_arr_all_sq /= len(coeff_list)  # <X^2>
+                    del(coeff_list)
+                    var = img_arr_all_sq - img_arr_all**2
+                    # If there's no variance, there's no point in using weights...
+                    if np.allclose(var, np.zeros_like(var)):
+                        raise Warning('No variance in images, ignoring variance')
+                        if return_unc:
+                            coeffs, _cov = np.polyfit(t_grid,
+                                                      img_arr_all.reshape(nframes_grid, -1),
+                                                      poly_order,
+                                                      cov=return_unc)
+                            err = np.sqrt(np.diagonal(_cov, axis1=0, axis2=1))
+                            self.unc = err.T
+                        else:
+                            coeffs = np.polyfit(t_grid,
+                                                img_arr_all.reshape(nframes_grid, -1),
+                                                poly_order)
+                        coeffs = coeffs.reshape(-1, npx0, npx1)
+                    else:
+                        wgt = 1./var
+                        # Multiplying weights and measurements
+                        wy = wgt*img_arr_all
+                        wy = wy.reshape(nframes_grid, -1)
+                        # Clear memory
+                        del(img_arr_all_sq)
+                        del(img_arr_all)
+                        # Here is where the linear algebra fun starts...
+                        # using the weighted least squares estimator
+                        # https://en.wikipedia.org/wiki/Weighted_least_squares
+                        V = np.vander(t_grid, poly_order+1)
+                        _aux1 = np.einsum('ij, ik, il', V, wgt.reshape(nframes_grid, -1), V)
+                        _aux1 = np.linalg.inv(_aux1.swapaxes(1, 0))  # (V^T W V)^-1
+                        _aux2 = np.einsum('ijk, lj', _aux1, V)  # (V^T W V)^-1 V^T
+                        return _aux1, _aux2, wy
+        #             coeffs = np.einsum('ijk, kl -> ji', _aux2,
+        #                                wy)/wy.shape[1]
+        #             if return_unc:
+        #                 err = np.sqrt(np.diagonal(_aux1, axis1=1, axis2=2))
+        #                 self.unc = err.T
+        #         # Ignore spread and consider all steps in the ramp with the same weight
+        #         else:
+        #             if return_unc:
+        #                 coeffs, _cov = np.polyfit(t_grid,
+        #                                           img_arr_all.reshape(nframes_grid, -1),
+        #                                           poly_order,
+        #                                           cov=return_unc)
+        #                 err = np.sqrt(np.diagonal(_cov, axis1=0, axis2=1))
+        #                 self.unc = err.reshape((poly_order+1, npx0, npx1))
+        #             else:
+        #                 coeffs = np.polyfit(t_grid, img_arr_all.reshape(nframes_grid, -1),
+        #                                     poly_order)
+        #         self.data = coeffs.reshape((poly_order+1, npx0, npx1))
+        #         if 'input_files' not in self.meta.keys():
+        #             # Use this or the uri from the asdf files?
+        #             self.meta['input_files'] = input_files
+        #     else:
+        #         raise Warning('Linearity images not fit, creating dummy files...')
+        #
+        # # Construct the linearity object from the data model.
+        # linearityfile = rds.LinearityRef()
+        # linearityfile['meta'] = self.meta
+        # linearityfile['coeffs'] = self.data
+        # linearityfile['unc'] = self.unc
+        # nonlinear_pixels = np.where((self.mask == float('NaN')) |
+        #                             (self.data[0, :, :] == float('NaN')))
+        # self.mask[nonlinear_pixels] = 2 ** 20  # linearity correction not available
+        # linearityfile['dq'] = self.mask
+        # # Add in the meta data and history to the ASDF tree.
+        # af = asdf.AsdfFile()
+        # af.tree = {'roman': linearityfile}
+        # af.write_to(self.outfile)
 
-    def fit_single(self, input_file, poly_order=5, constrained=False):
+    def fit_single(self, input_file, poly_order=5, constrained=False,
+                   return_time=False):
         """
         Method to fit the linearity coefficients for a single flat image.
 
@@ -75,7 +195,7 @@ class Linearity(ReferenceFile):
         input_file: str; File to use to obtain the linearity coefficients.
         poly_order: integer; Polynomial order to use for the fits.
         constrained: bool; If True, it will fix intercept to 0 and slope to 1.
-
+        return_time: bool; If True, it will return the time array.
         Outputs
         -------
         poly: (numpy.ndarray); Array containing the coefficients of the best-fit
@@ -91,7 +211,7 @@ class Linearity(ReferenceFile):
         time = img_in['roman']['meta']['exposure']['frame_time']
         img_arr = img_in['roman']['data']
         if np.isscalar(time):
-            time = np.ones(img_arr.shape[0])*time
+            time = np.arange(0, img_arr.shape[0])*time
         else:
             if time.shape[0] != img_arr.shape[0]:
                 raise ValueError('Frame times should have the same length as datacube')
@@ -129,7 +249,7 @@ class Linearity(ReferenceFile):
                 img_arr -= V[:, -1, None]
                 # Now drop that column from the Vandermonde matrix
                 V = np.delete(V, -1, axis=1)
-                coeffs, _ = np.linalg.leastsq(V, img_arr.reshape(nframes, -1))
+                coeffs, _ = np.linalg.leastsq(V, img_arr.reshape(nframes, -1), rcond=None)
                 coeffs = coeffs.reshape(-1, npix_0, npix_1)
                 # Insert the slope=1 and intercept=0
                 coeffs = np.insert(coeffs, poly_order-1,
@@ -137,22 +257,20 @@ class Linearity(ReferenceFile):
                 coeffs = np.insert(coeffs, poly_order,
                                    np.zeros(npix_0, npix_1))
         else:
-            # For loop over all pixels -- slow but safe
-            coeffs = np.zeros((poly_order+1, npix_0, npix_1))
-            for i in range(npix_0*npix_1):
-                ipx, jpx = np.unravel_index(i, (npix_0, npix_1))
-                dq_here = img_dq[:, ipx, jpx]
-                img_here = img_arr[:, ipx, jpx]
-                if np.count_nonzero(dq_here != 0) > poly_order:
-                    # There are more parameters than points to fit
-                    coeffs[:, ipx, jpx] = np.nan
-                    mask[ipx, jpx] = 2**20
-                else:
-                    # Fit the relevant frames
-                    frames_good = np.where(dq_here == 0)[0]
-                    aux_coeff = np.polyfit(frames_good, img_here[frames_good], deg=poly_order)
-                    coeffs[:, ipx, jpx] = aux_coeff
-        return coeffs, mask
+            if not constrained:
+                aux_arr = np.ma.array(img_arr)
+                aux_arr.mask = (img_dq != 0)
+                coeffs = np.ma.polyfit(time, aux_arr.reshape(nframes, -1), poly_order)
+                coeffs = coeffs.reshape(-1, npix_0, npix_1)
+            else:
+                raise NotImplementedError
+        # Mask bad pixels
+        mask[np.where(np.isnan(coeffs))[1:]] = 2**20
+
+        if return_time:
+            return coeffs, mask, time
+        else:
+            return coeffs, mask
 
 
 def get_fit_length(datacube, time, dq=None, frac_thr=0.5,
@@ -207,8 +325,12 @@ def get_fit_length(datacube, time, dq=None, frac_thr=0.5,
     if verbose:
         print('Standard deviation estimate:', std)
     if dq is None:
-        # Get the first frame at which there are some signs of saturation in a pixel
-        nframes = np.where(grad < nsigma*std)[0][0]  # TODO: check this with non-evenly spaced reads
+        try:
+            # Get the first frame at which there are some signs of saturation in a pixel
+            nframes = np.where(grad < nsigma*std)[0][0]  # TODO: check this with not even spacing
+        except IndexError:
+            # If there are no saturated frames, we use all of them
+            nframes = datacube.shape[0]
     else:
         # When applying a mask, it flattens the arrray, so we need to unravel the indices
         nframes = np.unravel_index(np.where(grad[dq == 0] < nsigma*std)[0][0], datacube.shape)[0]
