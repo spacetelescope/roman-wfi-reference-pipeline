dd303317d9e70ce6af80787b01bc267d206daf0f
Tyler Desjardins <desjard@stsci.edu>
2022-10-17 15:37:10 -0400

    Update to gain code. Changes to reference file base class for build 7 meta data changes. Incorporate hack day requests (dq flag definitions and ancillary data file).

--- src/wfi_reference_pipeline/utilities/reference_file.py
+++ src/wfi_reference_pipeline/utilities/reference_file.py
@@ -1,47 +1,73 @@
 import datetime
 import os
+import sys
+
+if sys.version_info < (3, 9):
+    import importlib_resources
+else:
+    import importlib.resources as importlib_resources
 
 from astropy.time import Time
 import yaml
 import numpy as np
+
+from romancal.lib import dqflags
 
 from wfi_reference_pipeline.version import version as PIPELINE_VERSION
 
 
 class ReferenceFile:
     """
-    Base class ReferenceFile() writes static meta data for all reference file types
+    Base class ReferenceFile() writes static metadata for all reference file types
     are written.
     """
 
-    def __init__(self, data, meta_data, bit_mask=None, clobber=False):
+    def __init__(self, data, meta_data={}, bit_mask=None, clobber=False,
+                 make_mask=False, mask_size=(4096, 4096)):
 
-        self.data = data
+        self.input_data = data
+
+        # Load DQ flag definitions from romancal
+        self.dqflag_defs = dqflags.pixel
 
         if np.shape(bit_mask):
             print("Mask provided. Skipping internal mask generation.")
             self.mask = bit_mask.astype(np.uint32)
         else:
-            self.mask = np.zeros((4096, 4096), dtype=np.uint32)
+            if make_mask:
+                self.mask = np.zeros(mask_size, dtype=np.uint32)
+            else:
+                self.mask = None
 
-        # Grab the meta data from the yaml file if provided.
-        if type(meta_data) is dict:
+        # Grab the metadata from the yaml file if provided.
+        if not meta_data:
+            meta_data = {}
+        if isinstance(meta_data, dict):
             self.meta = meta_data
-        else:
+        elif isinstance(meta_data, str):
             with open(meta_data) as md:
                 self.meta = yaml.safe_load(md)
+        else:
+            raise TypeError(f'meta_data should be of type dict '
+                            f'or str, but got {type(meta_data)}')
 
         # Convert use after date to Astropy.Time object.
         if 'useafter' in self.meta.keys():
             self.meta['useafter'] = Time(self.meta['useafter'])
         else:
             self.meta['useafter'] = Time(datetime.datetime.now())
-        # Write static meta data for all file type.
+        # Write static metadata for all file type.
         self.meta['author'] = f'WFI Reference File Pipeline version {PIPELINE_VERSION}'
-        self.meta['origin'] = 'STScI'
+        self.meta['origin'] = 'STSCI'
         self.meta['telescope'] = 'ROMAN'
         if 'instrument' in self.meta.keys():
             self.meta['instrument'].update({'name': 'WFI'})
+
+        # Ancillary data for reference file modules
+        with importlib_resources.path('wfi_reference_pipeline.resources.data',
+                                          'ancillary.yaml') as afile:
+            with open(afile) as af:
+                self.ancillary = yaml.safe_load(af)
 
         # Other stuff.
         self.clobber = clobber
