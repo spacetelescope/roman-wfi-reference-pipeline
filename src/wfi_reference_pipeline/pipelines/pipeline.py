b6ec8585ffc9ab6c0a7c5ec98e242a17e622737d
Bradley Sappington <bsappington@stsci.edu>
2024-06-07 13:14:36 -0400

    naming to data_files

--- src/wfi_reference_pipeline/pipelines/pipeline.py
+++ src/wfi_reference_pipeline/pipelines/pipeline.py
@@ -25,10 +25,10 @@ class Pipeline(ABC):
         try:
             # Initialize logging named for the derived class
             configure_logging(f"{self.__class__.__name__}")
-            self._datafiles_config = get_data_files_config()
-            self.ingest_path = Path(self._datafiles_config["ingest_dir"])
-            self.prep_path = Path(self._datafiles_config["prep_dir"])
-            self.pipeline_out_path = Path(self._datafiles_config["crds_ready_dir"])
+            self._data_files_config = get_data_files_config()
+            self.ingest_path = Path(self._data_files_config["ingest_dir"])
+            self.prep_path = Path(self._data_files_config["prep_dir"])
+            self.pipeline_out_path = Path(self._data_files_config["crds_ready_dir"])
         except (FileNotFoundError, ValueError) as e:
             print(f"ERROR READING CONFIG FILE - {e}")
             exit()
