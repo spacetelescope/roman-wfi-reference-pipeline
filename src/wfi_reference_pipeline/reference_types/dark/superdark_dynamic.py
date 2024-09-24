ebc6f82713a60e69ba81af5050095a73d790af59
Bradley Sappington <bsappington@stsci.edu>
2024-09-24 14:31:18 -0400

    move dev tools to appropriate file

--- src/wfi_reference_pipeline/reference_types/dark/superdark_dynamic.py
+++ src/wfi_reference_pipeline/reference_types/dark/superdark_dynamic.py
@@ -1,7 +1,6 @@
 import gc
 import logging
 import multiprocessing
-import os
 import time
 from datetime import datetime
 from multiprocessing import Process, Queue, shared_memory
@@ -390,30 +389,3 @@ class SuperDarkDynamic:
         af = asdf.AsdfFile()
         af.tree = {"meta": meta_superdark, "data": self.superdark}
         af.write_to(outfile)
-
-
-def get_mem_usage_gb():
-    """
-    Function to return memory usage throughout module.
-
-    Returns
-    ----------
-    memory_usage; float
-        Memory in Gigabytes being used.
-
-    """
-    memory_usage = psutil.virtual_memory().used / GB  # in GB
-    return memory_usage
-
-def get_process_memory_usage(self):
-    process = psutil.Process(os.getpid())
-    memory_info = process.memory_info()
-    rss = memory_info.rss  # Resident Set Size: physical memory used
-    vms = memory_info.vms  # Virtual Memory Size: total memory used, including swap
-    return rss, vms
-
-def log_process_memory_usage(self):
-    rss, vms = self.get_process_memory_usage()
-    logging.debug(
-        f"Memory usage: RSS={rss / (1024 ** 2):.2f} MB, VMS={vms / (1024 ** 2):.2f} MB"
-    )
