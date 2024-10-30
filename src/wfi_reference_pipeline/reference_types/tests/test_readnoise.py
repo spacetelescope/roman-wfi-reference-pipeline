8debbcf68114d9336d75570356cfa3bbad189503
Rick Cosentino <rcosenti@stsci.edu>
2024-10-30 10:17:52 -0400

    Updated readnoise tests for standardization

--- src/wfi_reference_pipeline/reference_types/tests/test_readnoise.py
+++ src/wfi_reference_pipeline/reference_types/tests/test_readnoise.py
@@ -4,7 +4,6 @@ from wfi_reference_pipeline.resources.make_test_meta import MakeTestMeta
 from wfi_reference_pipeline.constants import REF_TYPE_READNOISE, REF_TYPE_DARK
 from unittest.mock import MagicMock, patch
 import numpy as np
-import astropy.units as u
 
 
 @pytest.fixture
@@ -15,53 +14,59 @@ def valid_meta_data():
 
 
 @pytest.fixture
-def valid_ref_type_data():
-    """Fixture for generating valid ref_type_data (read noise image)."""
+def valid_ref_type_data_array():
+    """Fixture for generating valid ref_type_data array (read noise image)."""
     return np.random.random((4096, 4096))  # Simulate a valid read noise image
 
 
 @pytest.fixture
-def readnoise_object(valid_meta_data, valid_ref_type_data):
-    """Fixture for initializing a valid ReadNoise object."""
-    readnoise_obj = ReadNoise(meta_data=valid_meta_data, ref_type_data=valid_ref_type_data)
-    yield readnoise_obj
+def valid_ref_type_data_cube():
+    """Fixture for generating valid ref_type_data cube (read noise cube)."""
+    return np.random.random((3, 4096, 4096))  # Simulate a valid read noise image
 
 
 @pytest.fixture
-def valid_data_cube():
-    """Fixture for generating a mock data cube for testing ramp residual variance."""
-    mock_data_cube = MagicMock()
-    mock_data_cube.num_i_pixels = 4096
-    mock_data_cube.num_j_pixels = 4096
-    mock_data_cube.data = np.random.random((5, 4096, 4096))  # 5 reads in the cube
-    mock_data_cube.ramp_model = np.random.random((5, 4096, 4096))  # Simulated ramp model
-    return mock_data_cube
+def readnoise_object_with_data_array(valid_meta_data, valid_ref_type_data_array):
+    """Fixture for initializing a ReadNoise object with a valid data array."""
+    readnoise_object_with_data_array = ReadNoise(meta_data=valid_meta_data,
+                                                 ref_type_data=valid_ref_type_data_array)
+    yield readnoise_object_with_data_array
 
 
 @pytest.fixture
-def readnoise_object_with_data_cube(valid_meta_data, valid_data_cube):
+def readnoise_object_with_data_cube(valid_meta_data, valid_ref_type_data_cube):
     """Fixture for initializing a ReadNoise object with a valid data cube."""
-    readnoise_obj = ReadNoise(meta_data=valid_meta_data, ref_type_data=None)
-    readnoise_obj.data_cube = valid_data_cube  # Assign mock data cube
-    yield readnoise_obj
+    readnoise_object_with_data_cube = ReadNoise(meta_data=valid_meta_data,
+                                                ref_type_data=valid_ref_type_data_cube)
+    yield readnoise_object_with_data_cube
 
 
 class TestReadNoise:
 
-    def test_readnoise_instantiation_with_valid_data(self, readnoise_object):
+    def test_readnoise_instantiation_with_valid_ref_type_data_array(self,
+                                                                    readnoise_object_with_data_array):
         """
-        Test that ReadNoise object is created successfully with valid input data.
+        Test that ReadNoise object is created successfully with valid input data array.
         """
-        assert isinstance(readnoise_object, ReadNoise)
-        assert readnoise_object.readnoise_image.shape == (4096, 4096)
+        assert isinstance(readnoise_object_with_data_array, ReadNoise)
+        assert readnoise_object_with_data_array.readnoise_image.shape == (4096, 4096)
 
-    def test_readnoise_instantiation_with_invalid_metadata(self, valid_ref_type_data):
+    def test_readnoise_instantiation_with_valid_ref_type_data_cube(self,
+                                                                    readnoise_object_with_data_cube):
+        """
+        Test that ReadNoise object is created successfully with valid input data cube.
+        """
+        assert isinstance(readnoise_object_with_data_cube, ReadNoise)
+        assert readnoise_object_with_data_cube.data_cube is not None
+        assert readnoise_object_with_data_cube.readnoise_image is None  # Ensure image is not created yet
+
+    def test_readnoise_instantiation_with_invalid_metadata(self, valid_ref_type_data_array):
         """
         Test that ReadNoise raises TypeError with invalid metadata type.
         """
         bad_test_meta = MakeTestMeta(ref_type=REF_TYPE_DARK)
         with pytest.raises(TypeError):
-            ReadNoise(meta_data=bad_test_meta.meta_dark, ref_type_data=valid_ref_type_data)
+            ReadNoise(meta_data=bad_test_meta.meta_dark, ref_type_data=valid_ref_type_data_array)
 
     def test_readnoise_instantiation_with_invalid_ref_type_data(self, valid_meta_data):
         """
@@ -69,28 +74,6 @@ class TestReadNoise:
         """
         with pytest.raises(TypeError):
             ReadNoise(meta_data=valid_meta_data, ref_type_data='invalid_ref_data')
-
-    def test_readnoise_instantiation_with_data_cube(self, valid_meta_data):
-        """
-        Test that ReadNoise object handles 3D data cube input correctly.
-        """
-        data_cube = np.random.random((5, 4096, 4096))  # 5 reads in the cube
-        readnoise_obj = ReadNoise(meta_data=valid_meta_data, ref_type_data=data_cube)
-
-        assert readnoise_obj.data_cube is not None
-        assert readnoise_obj.readnoise_image is None  # Ensure image is not created yet
-        assert readnoise_obj.data_cube.data.shape == (5, 4096, 4096)
-
-    def test_readnoise_instantiation_with_quantity_object(self, valid_meta_data):
-        """
-        Test that ReadNoise object handles Quantity object as ref_type_data correctly.
-        """
-        data_quantity = (np.random.random((4096, 4096)) * u.electron)  # Quantity object
-        readnoise_obj = ReadNoise(meta_data=valid_meta_data, ref_type_data=data_quantity)
-
-        assert readnoise_obj.readnoise_image is not None
-        assert isinstance(readnoise_obj.readnoise_image, np.ndarray)
-        assert readnoise_obj.readnoise_image.shape == (4096, 4096)
 
     @patch("asdf.open")
     def test_readnoise_instantiation_with_file_list(self, mock_asdf_open, valid_meta_data):
@@ -113,25 +96,25 @@ class TestReadNoise:
 
         assert readnoise_obj.num_files == 2
 
-    def test_make_readnoise_image_using_cds_noise(self, readnoise_object):
+    def test_make_readnoise_image_using_cds_noise(self, readnoise_object_with_data_cube):
         """
         Test that make_readnoise_image can be modified to use CDS noise calculation.
         """
         # Mock the comp_cds_noise method to simulate CDS noise calculation
-        readnoise_object.comp_cds_noise = MagicMock(return_value='mock_cds_noise_image')
+        readnoise_object_with_data_cube.comp_cds_noise = MagicMock(return_value='mock_cds_noise_image')
 
         # Modify the method for CDS noise calculation
-        readnoise_object.readnoise_image = readnoise_object.comp_cds_noise()
+        readnoise_object_with_data_cube.readnoise_image = readnoise_object_with_data_cube.comp_cds_noise()
 
         # Assert that the comp_cds_noise method was called and readnoise_image was updated
-        readnoise_object.comp_cds_noise.assert_called_once()
-        assert readnoise_object.readnoise_image == 'mock_cds_noise_image'
+        readnoise_object_with_data_cube.comp_cds_noise.assert_called_once()
+        assert readnoise_object_with_data_cube.readnoise_image == 'mock_cds_noise_image'
 
-    def test_populate_datamodel_tree(self, readnoise_object):
+    def test_populate_datamodel_tree(self, readnoise_object_with_data_array):
         """
         Test that the data model tree is correctly populated in the ReadNoise object.
         """
-        data_model_tree = readnoise_object.populate_datamodel_tree()
+        data_model_tree = readnoise_object_with_data_array.populate_datamodel_tree()
 
         # Assuming the ReadNoise data model includes 'meta' and 'noise' (instead of 'dq')
         assert 'meta' in data_model_tree
@@ -141,9 +124,10 @@ class TestReadNoise:
         assert data_model_tree['data'].shape == (4096, 4096)
         assert data_model_tree['data'].dtype == np.float32  # Assuming read noise values are stored as floats
 
-    def test_readnoise_outfile_default(self, readnoise_object):
+    def test_readnoise_outfile_default(self, readnoise_object_with_data_array):
         """
-        Test that the default outfile name is correct in the ReadNoise object.
+        Test that the default outfile name is correct in the ReadNoise object with the assumption
+        that the default name is 'roman_readnoise.asdf'
         """
-        assert readnoise_object.outfile == "roman_readnoise.asdf"  # Assuming the default name is 'roman_readnoise.asdf'
+        assert readnoise_object_with_data_array.outfile == "roman_readnoise.asdf"
 
