d72e392899ef9f1d21a5f8af965bc6d5d113b4d0
Sierra Rosa Gomez <sigomez@stsci.edu>
2025-01-15 09:32:25 -0500

    initial commit for Mask(), implemented first version of code used for flags identified with flats.

--- src/wfi_reference_pipeline/reference_types/mask/mask.py
+++ src/wfi_reference_pipeline/reference_types/mask/mask.py
@@ -5,6 +5,8 @@ from wfi_reference_pipeline.resources.wfi_meta_mask import WFIMetaMask
 
 from ..reference_type import ReferenceType
 
+from . import mask_helpers as helper
+from roman_datamodels.dqflags import pixel as dqflags
 
 class Mask(ReferenceType):
     """
@@ -27,6 +29,12 @@ class Mask(ReferenceType):
         bit_mask=None,
         outfile="roman_mask.asdf",
         clobber=False,
+        boxwidth=15,
+        sigma_stats=3.,
+        dead_sigma=5.,
+        max_dead_signal=0.05,
+        max_low_qe_signal=0.5,
+        max_open_adj_signal=1.05
     ):
         """
         The __init__ method initializes the class with proper input variables needed by the ReferenceType()
@@ -36,18 +44,33 @@ class Mask(ReferenceType):
         ----------
         meta_data: Object; default = None
             Object of meta information converted to dictionary when writing reference file.
+
         file_list: List of strings; default = None
             List of file names with absolute paths. Intended for primary use during automated operations.
+
         ref_type_data: numpy array; default = None
             Input data cube. Intended for development support file creation or as input
             for reference file types not generated from a file list.
+
         bit_mask: 2D integer numpy array, default = None
             A 2D data quality integer mask array to be applied to reference file.
+
         outfile: string; default = roman_mask.asdf
             File path and name for saved reference file.
+
         clobber: Boolean; default = False
             True to overwrite outfile if outfile already exists. False will not overwrite and exception
             will be raised if duplicate file found.
+
+        boxwidth: int, default = 15
+            The width of the smoothing kernel.
+
+        sigma_stats: float, default = 3.
+            Sigma value for sigma clipping the master + normalized image.
+
+        dead_sigma: float, default = 5.
+            Sigma value used when determing DEAD pixels. Number of standard 
+            deviations below the mean at which a pixel is considered DEAD and DO_NOT_USE.
         ---------
 
         See reference_type.py base class for additional attributes and methods.
@@ -63,11 +86,27 @@ class Mask(ReferenceType):
             clobber=clobber
         )
 
+        self.boxwidth = boxwidth
+        self.sigma_stats = sigma_stats
+        self.dead_sigma = dead_sigma
+        self.max_dead_signal = max_dead_signal
+        self.max_low_qe_signal = max_low_qe_signal
+        self.max_open_adj_signal = max_open_adj_signal
+
+        ## if they add an array will they still need filelist,,, they're compyting
+        # Multiple flags are identified via normalized image
+        self.normalized_image = helper.create_normalized_slope_image(
+            filelist=self.file_list,
+            sigma=self.sigma_stats,
+            boxwidth=self.boxwidth
+            )
+
         # Default meta creation for module specific ref type.
         if not isinstance(meta_data, WFIMetaMask):
             raise TypeError(
                 f"Meta Data has reftype {type(meta_data)}, expecting WFIMetaMask."
             )
+
         if len(self.meta_data.description) == 0:
             self.meta_data.description = "Roman WFI mask reference file."
 
@@ -76,15 +115,16 @@ class Mask(ReferenceType):
         # Initialize attributes.
         self.mask_image = None
 
-        # Module flow creating reference file.
-        if not (isinstance(ref_type_data, np.ndarray) and
-                ref_type_data.dtype == np.uint32 and
-                ref_type_data.shape == (4096, 4096)):
-            raise ValueError("Mask ref_type_data must be a NumPy array of dtype uint32 and shape 4096x4096.")
-        else:
-            logging.debug("The input 2D data array is now self.mask_image.")
-            self.mask_image = ref_type_data
-            logging.debug("Ready to generate reference file.")
+        # # Module flow creating reference file.
+        # if not (isinstance(ref_type_data, np.ndarray) and
+        #         ref_type_data.dtype == np.uint32 and
+        #         ref_type_data.shape == (4096, 4096)):
+        #     raise ValueError("Mask ref_type_data must be a NumPy array of dtype uint32 and shape 4096x4096.")
+
+        # else:
+        #     logging.debug("The input 2D data array is now self.mask_image.")
+        #     self.mask_image = ref_type_data
+        #     logging.debug("Ready to generate reference file.")
 
     def make_mask_image(self):
         """
@@ -94,33 +134,100 @@ class Mask(ReferenceType):
         variables and parameters are set and this is the single call to populate all attributes needed
         for the reference file data model.
         """
+        # TODO add normalized image step...
 
         self._update_mask_ref_pixels()
-        self._add_random_bad_pixels()
+        self._update_mask_dead_pixels()
+        self._update_mask_low_qe_open_adj_pixels()
+
         self.mask_image = self.mask
 
     def _update_mask_ref_pixels(self):
         """
-        Create array to flag the 4 pixel reference poxel border around the detector.
+        Create array to flag the 4 pixel reference pixel border around the detector.
         """
-
         refpix_mask = np.zeros((4096, 4096), dtype=np.uint32)
-        refpix_mask[:4, :] = 2**31 # apply to the top 4 rows for every column
-        refpix_mask[-4:, :] = 2**31 # apply to the bottom 4 rows for every column
-        refpix_mask[:, :4] = 2**31
-        refpix_mask[:, -4:] = 2**31
+
+        refpix_mask[:4, :] = dqflags.REFERENCE_PIXEL.value
+        refpix_mask[-4:, :] = dqflags.REFERENCE_PIXEL.value
+        refpix_mask[:, :4] = dqflags.REFERENCE_PIXEL.value
+        refpix_mask[:, -4:] = dqflags.REFERENCE_PIXEL.value
+
         self.mask += refpix_mask
 
-    def _add_random_bad_pixels(self):
+    def _update_mask_dead_pixels(self):
         """
-        Method to add some randomly located bad pixels.
+        Identify the DEAD pixels using the normalized image.
         """
+        # Mean and stdev of the normalized master image
+        mean_norm, stdev_norm = helper.create_image_stats(
+            data=self.normalized_image,
+            sigma=self.sigma_stats
+        )
 
-        # Randomly assigning 750-850 pixels as bad pixels that dont interfere with reference pixels
-        rand_num_badpixels = np.random.randint(750, 850)
-        coords_x = np.random.randint(4, 4091, rand_num_badpixels)
-        coords_y = np.random.randint(4, 4091, rand_num_badpixels)
-        self.mask[coords_x, coords_y] += 2**0
+        # Threshold for pixel to be considered DEAD
+        threshold = mean_norm - (self.dead_sigma * stdev_norm)
+
+        # Getting the map of DEAD pixels where GOOD = 0 and DEAD = 1
+        dead_mask = (self.normalized_image < threshold).astype(np.uint32)
+
+        # Setting DEAD pixels to the actual dq flag value
+        dead_mask[dead_mask == 1] = dqflags.DEAD.value
+
+        # Padding the image with ref pixel border
+        dead_mask = helper.pad_with_ref_pixels(dead_mask)
+
+        self.mask += dead_mask
+
+    def _update_mask_low_qe_open_adj_pixels(self):
+        """
+        Using the normalized image, identify LOW_QE, OPEN/ADJ pixels.
+        """
+        normalized_image = self.normalized_image
+
+        # Empty arrays for the open/adj/low_qe
+        low_qe_map = np.zeros(normalized_image.shape)
+        open_map = np.zeros(normalized_image.shape)
+        adj_map = np.zeros(normalized_image.shape)
+
+        # A map of the locations of low signal pixels
+        # TODO: use the already ID'd DEAD pix, not some arbitrary 5% number
+        # If you use the 5% number, then you're gonna have to use that for DEAD ID step
+        low_sig_y, low_sig_x = np.where((normalized_image > self.max_dead_signal)
+                                      & (normalized_image < self.max_low_qe_signal))
+
+        # Going through each low signal pixel and determining type
+        for x, y in zip(low_sig_x, low_sig_y):
+
+            adj_pix = normalized_image[helper.get_adjacent_pix(x, y, normalized_image)]
+            all_adj = (adj_pix > self.max_open_adj_signal)
+
+            # TODO: there currently aren't specific flags for OPEN/ADJ pixels.
+            if all(all_adj):
+                adj_map[y-1:y+2, x-1:x+2] = dqflags.RESERVED_5.value
+                adj_map[y, x] = 0
+                open_map[y, x] = dqflags.RESERVED_6.value
+
+            else:
+                low_qe_map[y, x] = dqflags.LOW_QE.value
+
+        low_qe_map = helper.pad_with_ref_pixels(low_qe_map)
+        open_map = helper.pad_with_ref_pixels(open_map)
+        adj_map = helper.pad_with_ref_pixels(adj_map)
+
+        # Adding to mask
+        self.mask += low_qe_map.astype(np.uint32)
+        self.mask += open_map.astype(np.uint32)
+        self.mask += adj_map.astype(np.uint32)
+
+    def _update_mask_do_not_use_pixels(self, flags=["DEAD"]):
+        """
+        This function adds the DO_NOT_USE flag to pixels with the following flags:
+            DEAD
+        This may be updated in the future with more flags.
+        """
+        # Gonna implement this later...
+        return
 
     def calculate_error(self):
         """
---
+++ src/wfi_reference_pipeline/reference_types/mask/mask_helpers.py
@@ -0,0 +1,259 @@
+import numpy as np
+
+from astropy.stats import sigma_clip
+from astropy.convolution import convolve, Box2DKernel
+
+import asdf
+import roman_datamodels as rdm
+
+def create_image_stats(data, sigma):
+    """
+    Calculating the sigma-clipped mean and stdev images from a stack of inputted images.
+    """
+    clipped_cube = sigma_clip(data,
+                              sigma=sigma,
+                              axis=0,
+                              masked=False)
+
+    mean_image = np.nanmean(clipped_cube,
+                            axis=0)
+
+    stdev_image = np.nanstd(clipped_cube,
+                            axis=0)
+
+    return mean_image, stdev_image
+
+
+def pad_with_ref_pixels(image):
+    """
+    Pad the image with four rows and columns of (reference) pixels with value of zero.
+    """
+    padded_im = np.zeros((4096, 4096), dtype=np.uint32)
+    padded_im[4:-4, 4:-4] = image
+
+    return padded_im
+
+
+def remove_ref_pixel_border(image):
+    """
+    Remove the outer four columns and rows of (reference) pixels to return the science image.
+    """
+    return image[4:-4, 4:-4]
+
+
+def smooth_image(image, boxwidth):
+    """
+    Smooth the inputted image using Box2DKernel kernel at specified boxwidth.
+    """
+    smoothing_kernel = Box2DKernel(boxwidth)
+
+    smoothed_image = convolve(image,
+                              smoothing_kernel,
+                              boundary="fill",
+                              fill_value=np.nanmedian(image),
+                              nan_treatment="interpolate")
+
+    return smoothed_image
+
+
+# I see that Flat and Dark have their own DataCube classes, should this
+# be one of those? Potentially might need when identifying from Darks
+# # Below is a slightly cleaned up version of ramp-fitting code from Bernie R.
+# class Polynomial:
+#     def __init__(self, nz: int, degree: int = 1):
+#         self.nz = nz
+#         self.degree = degree
+#         self.z = np.arange(nz)
+
+#         # Precompute basis matrix and its pseudo-inverse
+#         self.B = np.vander(self.z, N=degree + 1, increasing=True)
+#         self.pinvB = np.linalg.pinv(self.B)
+#         self.B_x_pinvB = self.B @ self.pinvB
+
+#     def fit(self, D):
+#         """
+#         Fits the polynomial to the data.
+#         Returns the coefficients of the fit.
+#         """
+#         return (self.pinvB @ D.reshape(self.nz, -1)).reshape((-1, *D.shape[1:]))
+
+#     def model(self, D):
+#         """
+#         Models the data based on the polynomial fit.
+#         Returns the modeled data.
+#         """
+#         return (self.B_x_pinvB @ D.reshape(self.nz, -1)).reshape((-1, *D.shape[1:]))
+
+
+# def get_slope(data):
+#     """
+#     Extracts the slope (linear term) of the data using polynomial fitting.
+#     """
+#     P = Polynomial(data.shape[0], degree=1)
+
+#     # Extract the linear coefficient
+#     slope = P.fit(data)[1]
+
+#     return slope
+
+class Polynomial():
+    def __init__(self, nz:int, degree:int=1):
+        # Setup
+        self.nz = nz
+        self.z = np.arange(nz)
+        self.degree = degree
+        # Make basis matrix
+        self.B = np.empty((self.nz,degree+1))
+        for col in np.arange(degree+1):
+            self.B[:,col] = self.z**col
+        # Make the fitting matrix. This does least squares fitting
+        self.pinvB = np.linalg.pinv(self.B)
+        # Make the modeling matrix. This is used to model the data
+        # from the fit
+        self.B_x_pinvB = np.matmul(self.B, self.pinvB)
+        
+    # Fitter
+    def polyfit(self, D):
+        # Print a warning if not float32
+        # if D.dtype != np.float32:
+        #     print('Warning: This operation is faster using np.float32 input')
+        # Pick off dimensions
+        ny = D.shape[1]
+        nx = D.shape[2]
+        # Fit
+        R = np.matmul(self.pinvB, D.reshape(self.nz,-1)).reshape((-1,ny,nx))
+        return(R)
+    
+    # Modeler
+    def polyval(self, D):
+        # Print a warning if not float32
+        # if D.dtype != np.float32:
+        #     print('Warning: This operation is faster using np.float32 input')
+        ny = D.shape[1]
+        nx = D.shape[2]
+        # Model
+        M = np.matmul(self.B_x_pinvB, D.reshape(self.nz,-1)).reshape((-1,ny,nx))
+        return(M)
+    
+def get_slope(data):
+    sh = data.shape
+    nz = sh[0]
+    ny = sh[1]
+    nx = sh[2]
+    P = Polynomial(nz, 1)
+    S = np.empty((1,ny,nx))
+    S[0] = P.polyfit(data)[1]
+    return S[0, :,:]
+
+def create_master_slope_image(filelist, sigma):
+    """
+    Perform reference pixel correction and create slope image for each file in filelist.
+    Then, sigma-clip the stack to create a single, master slope image.
+    """
+    # Beginning by getting a list of slopes
+    slopes = []
+
+    for file in filelist:
+
+        with rdm.open(file) as data:
+            slope = get_slope(data.data.value)
+
+        slopes.append(slope)
+
+    # Creating a master slope image by sigma-clipping
+    master_slope, _ = create_image_stats(slopes,
+                                         sigma=sigma)
+
+    return master_slope
+
+
+def create_normalized_slope_image(filelist, sigma, boxwidth):
+    """
+    Create a normalized image by dividing the master-averaged slope image by
+    the smoothed image. Used for DEAD, LOW_QE, OPEN/ADJ.
+    """
+    master_slope = create_master_slope_image(filelist,
+                                             sigma=sigma)
+
+    # Removing reference pixel border from image
+    master_slope = remove_ref_pixel_border(master_slope)
+
+    # Smoothing the image with the given boxwidth kernelsize
+    smoothed_image = smooth_image(master_slope,
+                                  boxwidth=boxwidth)
+
+    # Returning the normalized image
+    return master_slope / smoothed_image
+
+
+def get_adjacent_pix(x_val, y_val, im):
+    """
+    Identify the pixels adjacent to a given pixel.
+    Ex, note that x are the returned coordinates.
+    [ ][x][ ]
+    [x][o][x]
+    [ ][x][ ]
+    """
+    y_dim, x_dim = im.shape
+
+    if ((x_val > 0) and (x_val < (x_dim-1))):
+
+        if ((y_val > 0) and (y_val < y_dim-1)):
+            adj_x = np.array([x_val, x_val+1, x_val, x_val-1])
+            adj_y = np.array([y_val+1, y_val, y_val-1, y_val])
+
+        elif y_val == 0:
+            adj_x = np.array([x_val, x_val+1, x_val-1])
+            adj_y = np.array([y_val+1, y_val, y_val])
+
+        elif y_val == (y_dim-1):
+            adj_x = np.array([x_val+1, x_val, x_val-1])
+            adj_y = np.array([y_val, y_val-1, y_val])
+
+    elif x_val == 0:
+
+        if ((y_val > 0) and (y_val < y_dim-1)):
+            adj_x = np.array([x_val, x_val+1, x_val])
+            adj_y = np.array([y_val+1, y_val, y_val-1])
+
+        elif y_val == 0:
+            adj_x = np.array([x_val, x_val+1])
+            adj_y = np.array([y_val+1, y_val])
+
+        elif y_val == (y_dim-1):
+            adj_x = np.array([x_val+1, x_val])
+            adj_y = np.array([y_val, y_val-1])
+
+    elif x_val == (x_dim-1):
+
+        if ((y_val > 0) and (y_val < y_dim-1)):
+
+            adj_x = np.array([x_val, x_val, x_val-1])
+            adj_y = np.array([y_val+1, y_val-1, y_val])
+
+        elif y_val == 0:
+
+            adj_x = np.array([x_val, x_val-1])
+            adj_y = np.array([y_val+1, y_val])
+
+        elif y_val == (y_dim-1):
+
+            adj_x = np.array([x_val, x_val-1])
+            adj_y = np.array([y_val-1, y_val])
+
+    return adj_y, adj_x
+
+
+# TODO should this be from romancal instead?
+# Existing function in basic_calibration's CalDCL but for Detector Characterization Lab data
+def subtract_reference_pixels(cube):
+    """
+    Subtract the reference pixel average from the cube.
+    """
+    ref_pix = np.concatenate(cube[:, :, :4],
+                             cube[:, :, -4:],
+                             axis=2)
+
+    new_cube = (cube.shape[0], cube)
+
+    return None
