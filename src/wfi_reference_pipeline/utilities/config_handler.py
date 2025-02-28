63564e6c870394a14f8805e4a67b9fe2fc53b920
Bradley Sappington <bsappington@stsci.edu>
2025-02-28 10:19:02 -0500

    no need for config_path.  remove

--- src/wfi_reference_pipeline/utilities/config_handler.py
+++ src/wfi_reference_pipeline/utilities/config_handler.py
@@ -11,9 +11,8 @@ from wfi_reference_pipeline.constants import (
 from wfi_reference_pipeline.utilities.schemas import CONFIG_SCHEMA, QC_CONFIG_SCHEMA, CRDS_CONFIG_SCHEMA, PIPELINES_CONFIG_SCHEMA
 
 
-def _find_config_file(config_filename, config_path=""):
+def _find_config_file(config_filename):
     """Find the config file using CONFIG path and our projects root directory
-    Only need to send in config_path if you do NOT want to use our default config files.
 
     Parameters
     ----------
@@ -21,26 +20,18 @@ def _find_config_file(config_filename, config_path=""):
         config file we're looking for within the CONFIG_PATH
 
     """
-    # User sent in config_path specifically
-    if len(config_path):
-        config_path = Path(config_path)
-        if config_path.is_file():
-            config_file_path = config_path
-        else:
-            config_file_path = config_path / config_filename
+    current_path = Path(__file__).resolve()
+    this_path = Path(current_path) / "pyproject.toml"
+    if this_path.is_file():
+        root_path = current_path
     else:
-        current_path = Path(__file__).resolve()
-        this_path = Path(current_path) / "pyproject.toml"
-        if this_path.is_file():
-            root_path = current_path
-        else:
-            # move up our directory path until we find pyproject.toml
-            root_path = next(
-                (path for path in current_path.parents if (path / "pyproject.toml").is_file()),
-                None,
-            )
-        # once we have our root path, grab the path to the config_file
-        config_file_path = root_path / CONFIG_PATH / config_filename
+        # move up our directory path until we find pyproject.toml
+        root_path = next(
+            (path for path in current_path.parents if (path / "pyproject.toml").is_file()),
+            None,
+        )
+    # once we have our root path, grab the path to the config_file
+    config_file_path = root_path / CONFIG_PATH / config_filename
 
     if not config_file_path.is_file():
         config_file_path = None # Config file not found
@@ -73,23 +64,20 @@ def _validate_config(config_file_dict, schema):
         )
 
 
-def _get_config(config_filename, config_path=""):
+def _get_config(config_filename):
     """Return a dictionary that holds the contents of config.yml
 
      Parameters
     ----------
     config_file : str, optional
         The name of the configuration file to load, by default "crds_submission_config.yml".
-    config_path : str, optional
-        The full path to the configuration file to load, by default empty and will find within repo file structure
-        This is intended for users who do NOT want to use our default configuration files.
 
     Returns
     -------
     settings : dict
         A dictionary that holds the contents of the config file.
     """
-    config_file_location = _find_config_file(config_filename, config_path=config_path)
+    config_file_location = _find_config_file(config_filename)
 
     if config_file_location is None:
         raise FileNotFoundError(
@@ -109,7 +97,7 @@ def _get_config(config_filename, config_path=""):
     return settings
 
 
-def get_logging_config(config_file="config.yml", config_path=""):
+def get_logging_config(config_file="config.yml"):
     """Get configuration settings from config.yml for logging
     Validate that the settings are in the correct format before returning
 
@@ -117,8 +105,6 @@ def get_logging_config(config_file="config.yml", config_path=""):
     ----------
     config_file : str, optional
         The name of the configuration file to load, by default "crds_submission_config.yml".
-    config_path : str, optional
-        The full path to the configuration file to load, by default empty and will find within repo file structure
 
     Returns
     -------
@@ -126,12 +112,12 @@ def get_logging_config(config_file="config.yml", config_path=""):
         A dictionary containing the configuration settings.
     """
     # Ensure the file has all the needed entries with expected data types
-    settings = _get_config(config_file, config_path)
+    settings = _get_config(config_file)
     _validate_config(settings, CONFIG_SCHEMA)
     return settings["logging"]
 
 
-def get_data_files_config(config_file="config.yml", config_path=""):
+def get_data_files_config(config_file="config.yml"):
     """Get configuration settings from config.yml for data_files
     Validate that the settings are in the correct format before returning
 
@@ -139,19 +125,17 @@ def get_data_files_config(config_file="config.yml", config_path=""):
     ----------
     config_file : str, optional
         The name of the configuration file to load, by default "crds_submission_config.yml".
-    config_path : str, optional
-        The full path to the configuration file to load, by default empty and will find within repo file structure
 
     Returns
     -------
     dict
         A dictionary containing the configuration settings.
     """
-    settings = _get_config(config_file, config_path)
+    settings = _get_config(config_file)
     _validate_config(settings, CONFIG_SCHEMA)
     return settings["data_files"]
 
-def get_pipelines_config(ref_type, config_file="pipelines_config.yml", config_path=""):
+def get_pipelines_config(ref_type, config_file="pipelines_config.yml"):
     """Get configuration settings from pipelines_config.yml for any reference type
     Validate that the settings are in the correct format before returning
 
@@ -161,8 +145,6 @@ def get_pipelines_config(ref_type, config_file="pipelines_config.yml", config_pa
         The defined reference type from constants.py
     config_file : str, optional
         The name of the configuration file to load, by default "crds_submission_config.yml".
-    config_path : str, optional
-        The full path to the configuration file to load, by default empty and will find within repo file structure
 
     Returns
     -------
@@ -174,7 +156,7 @@ def get_pipelines_config(ref_type, config_file="pipelines_config.yml", config_pa
     ValueError
         If the ref_type hasn't been implemented yet
     """
-    settings = _get_config(config_file, config_path)
+    settings = _get_config(config_file)
     _validate_config(settings, PIPELINES_CONFIG_SCHEMA)
     if ref_type == REF_TYPE_DARK:
         settings_type = "dark"
@@ -185,7 +167,7 @@ def get_pipelines_config(ref_type, config_file="pipelines_config.yml", config_pa
     return settings[settings_type]
 
 
-def get_quality_control_config(ref_type, config_file="quality_control_config.yml", config_path=""):
+def get_quality_control_config(ref_type, config_file="quality_control_config.yml"):
     """Get configuration settings from quality_control_config.yml for any reference type
     Validate that the settings are in the correct format before returning
 
@@ -195,8 +177,6 @@ def get_quality_control_config(ref_type, config_file="quality_control_config.yml
         The defined reference type from constants.py
     config_file : str, optional
         The name of the configuration file to load, by default "crds_submission_config.yml".
-    config_path : str, optional
-        The full path to the configuration file to load, by default empty and will find within repo file structure
 
     Returns
     -------
@@ -213,10 +193,10 @@ def get_quality_control_config(ref_type, config_file="quality_control_config.yml
     setting_type = ""
     try:
         if ref_type == REF_TYPE_DARK:
-            settings = _get_config(config_file, config_path)
+            settings = _get_config(config_file)
             setting_type = "dark_control"
         elif ref_type == REF_TYPE_READNOISE:
-            settings = _get_config(config_file, config_path)["readnoise_control"]
+            settings = _get_config(config_file)["readnoise_control"]
             setting_type = "readnoise_control"
         else:
             raise ValueError(
@@ -229,7 +209,7 @@ def get_quality_control_config(ref_type, config_file="quality_control_config.yml
     return settings[setting_type]
 
 
-def get_crds_submission_config(config_file="crds_submission_config.yml", config_path=""):
+def get_crds_submission_config(config_file="crds_submission_config.yml"):
     """
     Get configuration settings from crds_submission_config.yml.
 
@@ -237,8 +217,6 @@ def get_crds_submission_config(config_file="crds_submission_config.yml", config_
     ----------
     config_file : str, optional
         The name of the configuration file to load, by default "crds_submission_config.yml".
-    config_path : str, optional
-        The full path to the configuration file to load, by default empty and will find within repo file structure
 
     Returns
     -------
@@ -254,6 +232,6 @@ def get_crds_submission_config(config_file="crds_submission_config.yml", config_
     ValidationError
         If the configuration does not match the CRDS_CONFIG_SCHEMA.
     """
-    settings = _get_config(config_file, config_path)
+    settings = _get_config(config_file)
     _validate_config(settings, CRDS_CONFIG_SCHEMA)
     return settings
