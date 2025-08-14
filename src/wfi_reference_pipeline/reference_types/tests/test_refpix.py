af50f338608b6fceeeefe3e73ea063560998650f
Sarah Betti <sbetti@plrrtbtvac1.stsci.edu>
2025-08-14 21:26:52 +0000

    testing the test refpix and removing unittest

--- src/wfi_reference_pipeline/reference_types/tests/test_refpix.py
+++ src/wfi_reference_pipeline/reference_types/tests/test_refpix.py
@@ -1,14 +1,13 @@
 import os
-from unittest.mock import MagicMock
 
 import numpy as np
 import pytest
-from romancal.lib import dqflags
+import asdf
+from roman_datamodels import maker_utils as utils
 
 from wfi_reference_pipeline.constants import REF_TYPE_REFPIX, REF_TYPE_READNOISE
 from wfi_reference_pipeline.reference_types.referencepixel.referencepixel import ReferencePixel
 from wfi_reference_pipeline.resources.make_test_meta import MakeTestMeta
-from wfi_reference_pipeline.utilities.simulate_reads import simulate_dark_reads
 
 skip_on_github = pytest.mark.skipif(
     os.getenv("GITHUB_ACTIONS") == "true",
@@ -25,30 +24,26 @@ def valid_meta_data():
 def valid_ref_type_data_cube():
     """Fixture for generating valid ref_type_data cube (reference pixel dark cube).
     """
-    # from test data, assume a mean and std distribution on a per frame level of 4200 DN with std of 800 DN. Must have a 4224 by 4096 pixel frame (4096+128 rows to include amp 33).  
-    random_refpix_frame = np.random.normal(4200, 800, 4224*4096) 
-    # up the ramp, assume a std of 4.5 DN per pixel.  
-    random_refpix_exp = np.array([np.random.normal(random_refpix_frame, np.ones_like(random_refpix_frame)*4.5, 4224*4096) for i in np.arange(5)]).reshape((5,4224, 4096)).astype(int)
-
+    random_refpix_exp = np.random.random((3, 4096, 4224))
     return random_refpix_exp  # Simulate a valid refpix data cube
-
-
 
 @pytest.fixture
 def refpix_object_with_data_cube(valid_meta_data, valid_ref_type_data_cube):
-    """Fixture for initializing a ReferencePixel object with a valid data cube."""
+    """Fixture for initializing a ReferencePixel object with two valid data cube."""
     refpix_object_with_data_cube = ReferencePixel(meta_data=valid_meta_data,
-                                      ref_type_data=valid_ref_type_data_cube)
+                                      ref_type_data=np.array(valid_ref_type_data_cube))
     yield refpix_object_with_data_cube
 
-class TestRefPix:
 
+class TestRefPix:
     def test_refpix_instantiation_with_valid_ref_type_data_cube(self, refpix_object_with_data_cube):
         """
         Test that RefPix object is created successfully with valid input data cube.
         """
         assert isinstance(refpix_object_with_data_cube, ReferencePixel)
         assert refpix_object_with_data_cube.ref_type_data is not None
+        assert refpix_object_with_data_cube.ref_type_data.shape == (1, 3, 4096, 4224)
+
         assert refpix_object_with_data_cube.gamma is None  # Ensure gamma array is not created yet
         assert refpix_object_with_data_cube.zeta is None  # Ensure zeta array is not created yet
         assert refpix_object_with_data_cube.alpha is None  # Ensure alpha array is not created yet
@@ -68,63 +63,48 @@ class TestRefPix:
         with pytest.raises(TypeError):
             ReferencePixel(meta_data=valid_meta_data, ref_type_data='invalid_ref_data')
 
-    @patch("asdf.open")
-    def test_refpix_instantiation_with_file_list(self, mock_asdf_open, valid_meta_data):
+
+    def test_refpix_instantiation_with_file_list(self, valid_meta_data):
         """
         Test that RefPix object handles file list input correctly.
         """
-        # Create a mock for the file content with the expected structure
-        mock_asdf_file = MagicMock()
-        mock_asdf_file.tree = {
-            "roman": {
-                "data": np.zeros((5, 10, 10))  # Mocking data
-            }
-        }
-
-        # Set the mock to return this structure when asdf.open is called
-        mock_asdf_open.return_value.__enter__.return_value = mock_asdf_file
-
-        mock_file_list = ["file1.fits", "file2.fits"]
+  
+        mock_file_list = ["file1.asdf", "file2.asdf"]
         refpix_obj = ReferencePixel(meta_data=valid_meta_data, file_list=mock_file_list)
+        assert len(refpix_obj.file_list) == 2
+        assert isinstance(refpix_obj.file_list, list)
 
-        assert refpix_obj.num_files == 2
-
-    def test_get_data_cube_from_dark_file(self, valid_meta_data):
+    def test_get_data_cube_from_dark_file(self, tmp_path, valid_meta_data):
         """
         Test open data cube from input file list
         """
-        # Create a mock for the file content with the expected structure
-        mock_asdf_file = MagicMock()
-        mock_asdf_file.tree = {
-            "roman": {
-                "data": np.zeros((5, 4096, 4096)),  # Mocking data
-                "amp33": np.zeros((5, 128, 4096))
-            }
-        }
 
-        # Set the mock to return this structure when asdf.open is called
-        mock_asdf_open.return_value.__enter__.return_value = mock_asdf_file
-
-        mock_file_list = ["file1.fits"]
+        file_path = tmp_path / "test.asdf"
+        with asdf.AsdfFile() as af:
+            af.tree = {"roman": utils.mk_level1_science_raw(shape=(5, 4096, 4096))}
+            af.write_to(file_path)
+ 
+        mock_file_list = [file_path]
         refpix_obj = ReferencePixel(meta_data=valid_meta_data, file_list=mock_file_list)
-        refpix_data = refpix_obj.get_data_cube_from_dark_file(mock_file_list[0], skip_first_frame=False)
+        print(refpix_obj.file_list)
+        refpix_data = refpix_obj.get_data_cube_from_dark_file(refpix_obj.file_list[0], skip_first_frame=False)
 
-        assert refpix_data.shape == (5, 4224, 4096)
+        assert refpix_data.shape == (5, 4096, 4224)
         assert refpix_data.dtype == np.float64
 
-    def test_get_detector_name_from_dark_file_meta(self, valid_meta_data):
+
+    def test_get_detector_name_from_dark_file_meta(self, tmp_path, valid_meta_data):
         """
         Test open data cube from input file list
         """
         # Create a mock for the file content with the expected structure
-        mock_asdf_file = MagicMock()
-        mock_asdf_file.tree = {
-            "meta":{"instrument":{"detector":'WFI01'}} }
+        file_path = tmp_path / "test.asdf"
+        with asdf.AsdfFile() as af:
+            af.tree = {"roman": utils.mk_level1_science_raw(shape=(1, 4096, 4096))}
+            af.tree['roman']['meta']['instrument']['detector'] = 'WFI01'
+            af.write_to(file_path)
 
-        # Set the mock to return this structure when asdf.open is called
-        mock_asdf_open.return_value.__enter__.return_value = mock_asdf_file
-
-        mock_file_list = ["file1.fits"]
+        mock_file_list = [file_path]
         refpix_obj = ReferencePixel(meta_data=valid_meta_data, file_list=mock_file_list)
         refpix_obj._get_detector_name_from_dark_file_meta(mock_file_list[0])
 
@@ -132,35 +112,27 @@ class TestRefPix:
 
 
     def test_make_referencepixel_image(self, refpix_object_with_data_cube):
-        mock_return_image = valid_ref_type_data_cube()
-        # make 2 exposures
-        refpix_object_with_data_cube.ref_type_data = [mock_return_image,mock_return_image]
-
         # Assert that make_referencepixel_image was called with tmppath=None (default)
-        refpix_object_with_data_cube.make_referencepixel_image.assert_called_once_with(tmppath=None)
-
-        refpix_object_with_data_cube.make_referencepixel_image()
-
+        refpix_object_with_data_cube.make_referencepixel_image(tmppath=None, detector_name='WFI01')
+        
         # Check that dark_rate_image and dark_rate_image_error are set
         assert refpix_object_with_data_cube.gamma is not None
         assert refpix_object_with_data_cube.zeta is not None
         assert refpix_object_with_data_cube.alpha is not None
 
-        assert refpix_object_with_data_cube.gamma.shape == (33, 286721)
-        assert refpix_object_with_data_cube.zeta.shape == (33, 286721)
-        assert refpix_object_with_data_cube.alpha.shape == (33, 286721)
+        assert refpix_object_with_data_cube.gamma.shape == (32, 286721)
+        assert refpix_object_with_data_cube.zeta.shape == (32, 286721)
+        assert refpix_object_with_data_cube.alpha.shape == (32, 286721)
     
 
-    # @skip_on_github
-    def test_populate_datamodel_tree(self, refpix_object_with_data_cube,
-                                     valid_ref_type_data_cube,
-                                     dark_rate_image_3_by_3):
+    @skip_on_github
+    def test_populate_datamodel_tree(self, refpix_object_with_data_cube):
         """
         Test that the data model tree is correctly populated in the Dark object.
         """
-        refpix_object_with_data_cube.gamma = np.zeros((33, 286721), dtype=complex)
-        refpix_object_with_data_cube.zeta = np.zeros((33, 286721), dtype=complex)
-        refpix_object_with_data_cube.alpha = np.zeros((33, 286721), dtype=complex)
+        refpix_object_with_data_cube.gamma = np.zeros((32, 286721), dtype=complex)
+        refpix_object_with_data_cube.zeta = np.zeros((32, 286721), dtype=complex)
+        refpix_object_with_data_cube.alpha = np.zeros((32, 286721), dtype=complex)
         data_model_tree = refpix_object_with_data_cube.populate_datamodel_tree()
 
         # Assuming the RefPix data model includes:
@@ -170,13 +142,13 @@ class TestRefPix:
         assert 'alpha' in data_model_tree
 
         # Check the shape and dtype of the 'gamma' array
-        assert data_model_tree['gamma'].shape == (33, 286721)
+        assert data_model_tree['gamma'].shape == (32, 286721)
         assert data_model_tree['gamma'].dtype == np.complex128
         # Check the shape and dtype of the 'zeta' array
-        assert data_model_tree['zeta'].shape == (33, 286721)
+        assert data_model_tree['zeta'].shape == (32, 286721)
         assert data_model_tree['zeta'].dtype == np.complex128
         # Check the shape and dtype of the 'alpha' array
-        assert data_model_tree['alpha'].shape == (33, 286721)
+        assert data_model_tree['alpha'].shape == (32, 286721)
         assert data_model_tree['alpha'].dtype == np.complex128
 
 
@@ -185,4 +157,4 @@ class TestRefPix:
         Test that the default outfile name is correct in the RefPix object with the assumption
         that the default name is 'roman_refpix.asdf'
         """
-        assert refpix_object_with_data_cube.outfile == "roman_refpix.asdf"
+        assert refpix_object_with_data_cube.outfile == "roman_referencepixel.asdf"
